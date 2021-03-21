411ba4f04b3074ceabde6ad0f57e58daaf88fdbf
yeonjuyeong <yeonjuyeong@gmail.com>
2021-03-21 17:37:56 +0900

    configure setup settings

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 sudo pip install ansible
 sudo pip install awscli  
+
+ansible-galaxy collection install community.general
 
 sudo ansible-playbook main_playbook.yaml
 
