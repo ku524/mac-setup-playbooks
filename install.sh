761ec821fef5dce8797957bb8fea4ae42babb8f1
ku524 <yeonjuyeong@gmail.com>
2021-04-12 14:41:38 +0900

    fix the bugs

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
-sudo pip install ansible
-sudo pip install awscli  
+sudo python3 get-pip.py
+sudo pip3 install ansible
+sudo pip3 install awscli  
 
 ansible-galaxy collection install community.general
 
-sudo ansible-playbook main_playbook.yaml -i inventory
+sudo ansible-playbook -i inventory mainplaybook.yml
 
 
