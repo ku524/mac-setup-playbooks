f76687f934bdcd27d1eff7212e6a9c3956250c50
ku524 <yeonjuyeong@gmail.com>
2021-12-27 22:43:58 +0900

    temp

--- wsl/install.sh
+++ wsl/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-sudo apt-get install build-essential procps curl file git -y
+sudo apt update
+sudo apt install build-essential procps curl file git python3 python3-pip zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+
 sudo pip3 install ansible
-sudo pip3 install awscli  
+sudo pip3 install awscli
 
 ansible-galaxy collection install community.general
 
-ansible-playbook -i inventory nonsudo_mainplaybook.yml
-sudo ansible-playbook -i inventory mainplaybook.yml
+sudo ansible-playbook -i inventory playbooks/apt.yml
+ansible-playbook -i inventory mainplaybook.yml
